3490d6dd9de576534b5cb7ba8a4503aa5a4c131f
Thomas David <contact@code-geek.de>
2021-05-05 17:15:37 +0200

    Handle paths with whitespaces
    Handle paths with whitespaces
    
    Handle paths with whitespaces

--- webnlg/test_graph2text.sh
+++ webnlg/test_graph2text.sh
@@ -7,25 +7,25 @@ CHECK_POINT=$2
 MODEL=$1
 FOLDER=outputs/test_model
 
-export CURRENT_DIR=${ROOT_DIR}
-export OUTPUT_DIR=${CURRENT_DIR}/${FOLDER}
+export CURRENT_DIR="${ROOT_DIR}"
+export OUTPUT_DIR="${CURRENT_DIR}/${FOLDER}"
 
-rm -rf $OUTPUT_DIR
-mkdir -p $OUTPUT_DIR
+rm -rf "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"
 
 export OMP_NUM_THREADS=10
 
 
 export CUDA_VISIBLE_DEVICES=${GPUID}
 #--n_test 20 \
-python ${ROOT_DIR}/finetune.py \
---data_dir=${ROOT_DIR}/data/webnlg \
+python "${ROOT_DIR}/finetune.py" \
+--data_dir="${ROOT_DIR}/data/webnlg" \
 --task graph2text \
---model_name_or_path=${MODEL} \
+--model_name_or_path="${MODEL}" \
 --eval_batch_size=8 \
 --gpus 1 \
---output_dir=$OUTPUT_DIR \
---checkpoint=$CHECK_POINT \
+--output_dir="$OUTPUT_DIR" \
+--checkpoint="$CHECK_POINT" \
 --max_source_length=384 \
 --max_target_length=384 \
 --val_max_target_length=384 \
